da598f15f636149b274a814a188ff1d4e3c13f1a
Myos <myos.chen@gmail.com>
2023-10-22 15:36:00 +0800

    chore: migrate from cra to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import './index.css';
-import '@fontsource/noto-sans/300.css';
-import '@fontsource/noto-sans/400.css';
-import '@fontsource/noto-sans/500.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import ProviderWrapper from './components/provider-wrapper';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import "@fontsource/noto-sans/300.css";
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/500.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ProviderWrapper from "./components/provider-wrapper";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -18,8 +17,3 @@ root.render(
     </ProviderWrapper>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

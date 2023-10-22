dca82808dcb1eeca81b0f0c1d582aa20dd427a7c
Myos <myos.chen@gmail.com>
2023-10-22 15:55:09 +0800

    chore: update eslint, prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/** @type {import('eslint').Linter.Config'} */
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      {allowConstantExport: true},
+    ],
+  },
+};
